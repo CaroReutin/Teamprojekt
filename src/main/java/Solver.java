0843eff4dbe8f270a4234bad9fb685fb2387691c
RobinIsTaken <robinistaken@users.noreply.github.com>
2022-12-05 15:41:56 +0100

    Add Prototype Solver

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+
 public class Solver {
+
+    public String solveGreedy(ArrayList<Item> items, ArrayList<Integer> amount, int capacity){
+        int size = items.size();
+        double[] ratio = new double[size];
+        int[] indexes = new int[size];
+
+        StringBuilder result = new StringBuilder();
+        String tmpString;
+
+        for (int i = 0; i < size; i++) {
+            if (items.get(i).getWeight() != 0 && items.get(i).getValue() != 0){
+                ratio[i] = (double)items.get(i).getValue() / (double)items.get(i).getWeight();
+                indexes[i] = i;
+            }else if(items.get(i).getValue() == 0){
+                ratio[i] = -1;
+            }else if (items.get(i).getWeight() == 0){
+                ratio[i] = -1;
+                tmpString = i + ",";
+                result.append(tmpString.repeat(amount.get(i)));
+            }
+        }
+
+        for (int i = 0; i < size-1; i++) {
+            for (int j = i; j < size; j++) {
+                if (ratio[i] < ratio[j]){
+                    double tmp = ratio[i];
+                    ratio[i] = ratio[j];
+                    ratio[j] = tmp;
+
+                    int temp = indexes[i];
+                    indexes[i] = indexes[j];
+                    indexes[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            tmpString = indexes[i] + ",";
+            for (int j = 0; j < amount.get(indexes[i]); j++) {
+                if (capacity >= items.get(indexes[i]).getWeight() && ratio[i] >= 0){
+                    capacity = capacity - items.get(indexes[i]).getWeight();
+                    result.append(tmpString);
+                }else {
+                    break;
+                }
+            }
+        }
+        if (result.length() > 0){
+            return result.substring(0,result.length()-1);
+        }
+        return "";
+    }
 }
---
+++ src/test/TestGreedy.java
@@ -0,0 +1,80 @@
+import Rucksack.Item;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+public class TestGreedy {
+    Solver s = new Solver();
+    ArrayList<Integer> amount;
+    ArrayList<Item> items;
+
+    @BeforeEach
+    public void cleanup(){
+        amount = new ArrayList<>();
+        items = new ArrayList<>();
+    }
+
+    @Test
+    public void optimalSolutions(){
+        amount.add(1);
+        amount.add(1);
+        amount.add(1);
+        // Sum of Weights = Capacity
+        items.add(new Item(10,1,"1"));
+        items.add(new Item(15,2,"2"));
+        items.add(new Item(40,3,"3"));
+        Assertions.assertEquals("2,0,1",s.solveGreedy(items,amount,6));
+        // None Fit
+        items = new ArrayList<>();
+        items.add(new Item(10,7,"1"));
+        items.add(new Item(15,8,"2"));
+        items.add(new Item(40,9,"3"));
+        Assertions.assertEquals("",s.solveGreedy(items,amount,6));
+        // Random Numbers
+        items = new ArrayList<>();
+        items.add(new Item(60,10,"1"));
+        items.add(new Item(130,20,"2"));
+        items.add(new Item(200,30,"3"));
+        Assertions.assertEquals("2,1",s.solveGreedy(items,amount,50));
+    }
+
+    @Test
+    public void suboptimalSolutions(){
+        amount.add(1);
+        amount.add(1);
+        amount.add(1);
+        // Random Numbers
+        items = new ArrayList<>();
+        items.add(new Item(60,10,"1"));
+        items.add(new Item(100,20,"2"));
+        items.add(new Item(120,30,"3"));
+        Assertions.assertEquals("0,1",s.solveGreedy(items,amount,50));
+    }
+
+    @Test
+    public void badInputs(){
+        amount.add(1);
+        amount.add(1);
+        amount.add(1);
+        // Weight 0
+        items = new ArrayList<>();
+        items.add(new Item(60,0,"1"));
+        items.add(new Item(100,40,"2"));
+        items.add(new Item(120,30,"3"));
+        Assertions.assertEquals("0,2",s.solveGreedy(items,amount,50));
+        // Value 0
+        items = new ArrayList<>();
+        items.add(new Item(60,10,"1"));
+        items.add(new Item(100,20,"2"));
+        items.add(new Item(0,30,"3"));
+        Assertions.assertEquals("0,1",s.solveGreedy(items,amount,50));
+        // Weight and Value 0
+        items = new ArrayList<>();
+        items.add(new Item(60,10,"1"));
+        items.add(new Item(100,20,"2"));
+        items.add(new Item(0,0,"3"));
+        Assertions.assertEquals("0,1",s.solveGreedy(items,amount,50));
+    }
+
+    //TODO tests for amount not just 1s
+}
