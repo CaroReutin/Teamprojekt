0d10e062522ec34e0f6b291d52badecb8c28f1d2
RobinIsTaken <robinistaken@users.noreply.github.com>
2022-12-08 18:09:12 +0100

    Implement options window

--- src/main/java/GUIFrontpage.java
+++ src/main/java/GUIFrontpage.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class GUIFrontpage {
 
@@ -48,15 +49,56 @@ public class GUIFrontpage {
         JButton settingsButton = new JButton("Einstellungen");
         settingsButton.setBounds(190, 260, 120, 40);
         pane.add(settingsButton);
+        createOptionsmenu(frame);
 
         settingsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                options.setLocation(frame.getLocation());
+                passwordInput.setValue("");
+                options.setVisible(true);
             }
         });
-
     }
 
+    private JDialog options;
+    private JFormattedTextField passwordInput;
+    private void createOptionsmenu(JFrame parent) {
+        options = new JDialog(parent,true);
+        options.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        Container optionsPane = options.getContentPane();
+        optionsPane.setLayout(null);
+        JButton close = new JButton("Close");
+        passwordInput = new JFormattedTextField("");
+        JButton enterPassword = new JButton("Enter");
+        options.setTitle("Einstellungen");
+        // Bugged: The first time Enter is pressed is always ignored, every other time the program works
+        options.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER,0),"callConfirmPassword");
+        options.getRootPane().getActionMap().put("callConfirmPassword", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                confirmPassword(passwordInput.getText(),options);
+            }
+        });
+        options.setSize(new Dimension(500,500));
+        passwordInput.setBounds(3*options.getWidth()/10,options.getHeight()/10, 4*options.getWidth()/10,options.getHeight()/10);
+        enterPassword.setBounds(4*options.getWidth()/10,2*options.getHeight()/10 + passwordInput.getHeight(), 2*options.getWidth()/10,options.getHeight()/10);
+        enterPassword.addActionListener(e -> confirmPassword(passwordInput.getText(),options));
+        close.setBounds(4*options.getWidth()/10,3*options.getHeight()/10 + passwordInput.getHeight() + enterPassword.getHeight(), 2*options.getWidth()/10,options.getHeight()/10);
+        close.addActionListener(e -> options.dispose());
 
+        optionsPane.add(passwordInput);
+        optionsPane.add(enterPassword);
+        optionsPane.add(close);
+    }
+
+    private static void confirmPassword(String pw,JDialog parent){
+        if (pw.matches(GUIManager.getPasswords().get(0))){
+            JOptionPane.showMessageDialog(parent,"Hinweise sind nun freigeschalten.","Erfolg",JOptionPane.INFORMATION_MESSAGE);
+
+            //Level.tipsAllowed(true);
+        }
+    }
 }
+
+
--- src/main/java/GUIManager.java
+++ src/main/java/GUIManager.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
 
 public class GUIManager {
+    // for now i'll put it here, not sure where it is most appropriate to put
+    private static ArrayList<String> passwords = new ArrayList<>();
+    public static ArrayList<String> getPasswords(){
+        return passwords;
+    }
+    // see comment above
+
+    private void innit() {
+        passwords.add("tipTop");
+    }
+
     private LevelManager lm = new LevelManager();
     GUIFrontpage guiFrontpage = new GUIFrontpage();
 
     public void launch(){
-       guiFrontpage.openProgrammWindow();
+        innit();
+        guiFrontpage.openProgrammWindow();
     }
 }
