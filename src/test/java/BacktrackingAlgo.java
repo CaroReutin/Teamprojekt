4d7e2701de602dc70eac16bf08e618b091590cea
RobinIsTaken <robinistaken@users.noreply.github.com>
2023-01-21 10:33:14 +0100

    add tests to backtracking

--- src/test/java/BacktrackingAlgo.java
+++ src/test/java/BacktrackingAlgo.java
@@ -3,24 +3,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import rucksack.Item;
+import rucksack.Level;
 import solving.SolverBacktracking;
+import solving.SolverGreedy;
 
 public class BacktrackingAlgo {
 
   SolverBacktracking solverBacktracking = new SolverBacktracking();
-  ArrayList<Integer> amount;
-  ArrayList<Item> items;
-  ArrayList<Item> expectedItems;
-  Integer expectedWeight;
-  Integer expectedValue;
+  ArrayList<Integer> amount = new ArrayList<>();
+  ArrayList<Item> items = new ArrayList<>();
+  ArrayList<Item> expectedItems = new ArrayList<>();
 
   @BeforeEach
   public void cleanup(){
     amount = new ArrayList<>();
     items = new ArrayList<>();
     expectedItems = new ArrayList<>();
-    expectedValue = 0;
-    expectedWeight = 0;
   }
 
   @Test
@@ -36,15 +34,9 @@ public class BacktrackingAlgo {
     expectedItems.add(items.get(1));
     expectedItems.add(items.get(2));
 
-    expectedValue = 28;
-    expectedWeight = 10;
-
     int capacity = 10;
 
-    //Assertions.assertEquals(SolverBacktracking.sortLikeBacktracking(expectedItems),
-    //solverBacktracking.solveBacktracking(items, amount, 10));
     Assertions.assertEquals(expectedItems, solverBacktracking.solveBacktracking(items,amount,capacity));
-    //Assertions.assertEquals(expectedWeight, solverBacktracking.getWeightCorrect(items, amount, 10));
   }
 
 
@@ -64,15 +56,8 @@ public class BacktrackingAlgo {
     expectedItems.add(items.get(2));
     expectedItems.add(items.get(3));
 
-    expectedWeight = 8;
-    expectedValue = 15;
-
     int capacity = 10;
-    //Assertions.assertEquals(SolverBacktracking.sortLikeBacktracking(expectedItems),
-      //solverBacktracking.solveBacktracking(items, amount, 10));
-    //Assertions.assertEquals(expectedValue, solverBacktracking.getValueCorrect(items, amount, 10));
     Assertions.assertEquals(expectedItems, solverBacktracking.solveBacktracking(items,amount,capacity));
-    //Assertions.assertEquals(expectedWeight, solverBacktracking.getWeightCorrect(items, amount, 10));
   }
 
   @Test
@@ -91,15 +76,8 @@ public class BacktrackingAlgo {
 
     expectedItems.add(items.get(0));
 
-    expectedValue = 31;
-    expectedWeight = 19;
-
     int capacity = 20;
-    //Assertions.assertEquals(SolverBacktracking.sortLikeBacktracking(expectedItems),
-    //solverBacktracking.solveBacktracking(items, amount, 10));
-    //Assertions.assertEquals(expectedValue, solverBacktracking.getValueCorrect(items, amount, 20));
     Assertions.assertEquals(expectedItems, solverBacktracking.solveBacktracking(items,amount,capacity));
-    //Assertions.assertEquals(expectedWeight, solverBacktracking.getWeightCorrect(items, amount, 20));
   }
 
   @Test
@@ -119,15 +97,9 @@ public class BacktrackingAlgo {
     expectedItems.add(items.get(1));
     expectedItems.add(items.get(3));
 
-    expectedValue = 18;
-    expectedWeight = 11;
     int capacity = 14;
 
-    //Assertions.assertEquals(SolverBacktracking.sortLikeBacktracking(expectedItems),
-    //solverBacktracking.solveBacktracking(items, amount, 10));
-    //Assertions.assertEquals(expectedValue, solverBacktracking.getValueCorrect(items, amount, 14));
     Assertions.assertEquals(expectedItems, solverBacktracking.solveBacktracking(items,amount,capacity));
-    //Assertions.assertEquals(expectedWeight, solverBacktracking.getWeightCorrect(items, amount, 14));
   }
 
   @Test
@@ -145,21 +117,11 @@ public class BacktrackingAlgo {
     amount.add(1);
 
     expectedItems.add(items.get(1));
-    //expectedItems.add(items.get(2));
     expectedItems.add(items.get(3));
     expectedItems.add(items.get(4));
 
-    //expectedValue = 22;
-    //expectedWeight = 10;
-    expectedValue = 23;
-    expectedWeight = 19;
-
     int capacity = 20;
-    //Assertions.assertEquals(SolverBacktracking.sortLikeBacktracking(expectedItems),
-    //solverBacktracking.solveBacktracking(items, amount, 10));
-    //Assertions.assertEquals(expectedValue, solverBacktracking.getValueCorrect(items, amount, 20));
     Assertions.assertEquals(expectedItems, solverBacktracking.solveBacktracking(items,amount,capacity));
-    //Assertions.assertEquals(expectedWeight, solverBacktracking.getWeightCorrect(items, amount, 20));
   }
 
   @Test
@@ -180,15 +142,8 @@ public class BacktrackingAlgo {
     expectedItems.add(items.get(2));
     expectedItems.add(items.get(3));
 
-    expectedValue = 30;
-    expectedWeight = 25;
-
     int capacity = 25;
-    //Assertions.assertEquals(SolverBacktracking.sortLikeBacktracking(expectedItems),
-    //solverBacktracking.solveBacktracking(items, amount, 10));
-    //Assertions.assertEquals(expectedValue, solverBacktracking.getValueCorrect(items, amount, 25));
     Assertions.assertEquals(expectedItems, solverBacktracking.solveBacktracking(items,amount,capacity));
-    //Assertions.assertEquals(expectedWeight, solverBacktracking.getWeightCorrect(items, amount, 25));
   }
 
   @Test
@@ -211,18 +166,176 @@ public class BacktrackingAlgo {
     expectedItems.add(items.get(4));
     expectedItems.add(items.get(5));
 
-    expectedValue = 59;
-    expectedWeight = 42;
-
     int capacity = 42;
 
-    //Assertions.assertEquals(SolverBacktracking.sortLikeBacktracking(expectedItems),
-    //solverBacktracking.solveBacktracking(items, amount, 10));
-    //Assertions.assertEquals(expectedValue, solverBacktracking.getValueCorrect(items, amount, 42));
     Assertions.assertEquals(expectedItems, solverBacktracking.solveBacktracking(items,amount,capacity));
-    //Assertions.assertEquals(expectedWeight, solverBacktracking.getWeightCorrect(items, amount, 42));
   }
 
+  @Test
+  public void whereGreedyHadOptimalSolutions(){
+    amount.add(1);
+    amount.add(1);
+    amount.add(1);
+    // Sum of Weights = Capacity
+    items.add(new Item(10,1,"1"));
+    items.add(new Item(15,2,"2"));
+    items.add(new Item(40,3,"3"));
+    Level level = new Level(items,amount,0,6);
+    expectedItems.add(items.get(2));
+    expectedItems.add(items.get(0));
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // None Fit
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(10,7,"1"));
+    items.add(new Item(15,8,"2"));
+    items.add(new Item(40,9,"3"));
+    level = new Level(items,amount,0,6);
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Best does not Fit
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(10,3,"1"));
+    items.add(new Item(15,4,"2"));
+    items.add(new Item(999999,9,"3"));
+    level = new Level(items,amount,0,6);
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Random Numbers
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(130,20,"2"));
+    items.add(new Item(200,30,"3"));
+    level = new Level(items,amount,0,50);
+    expectedItems.add(items.get(2));
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+  }
 
+  @Test
+  public void whereGreedyHadSuboptimalSolutions(){
+    amount.add(1);
+    amount.add(1);
+    amount.add(1);
+    // Random Numbers
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(100,20,"2"));
+    items.add(new Item(120,30,"3"));
+    Level level = new Level(items,amount,0,50);
+    expectedItems.add(items.get(2));
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+  }
+
+  @Test
+  public void badInputs(){
+    amount.add(1);
+    amount.add(1);
+    amount.add(1);
+    // Weight 0
+    items.add(new Item(60,0,"1"));
+    items.add(new Item(100,40,"2"));
+    items.add(new Item(120,30,"3"));
+    Level level = new Level(items,amount,0,50);
+    expectedItems.add(items.get(0));
+    expectedItems.add(items.get(2));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Value 0
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(100,20,"2"));
+    items.add(new Item(0,30,"3"));
+    level = new Level(items,amount,0,50);
+    expectedItems.add(items.get(0));
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Weight and Value 0
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(100,20,"2"));
+    items.add(new Item(0,0,"3"));
+    level = new Level(items,amount,0,50);
+    expectedItems.add(items.get(0));
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Weight and Value Negative
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(100,20,"2"));
+    items.add(new Item(-10,-20,"3"));
+    level = new Level(items,amount,0,50);
+    expectedItems.add(items.get(0));
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Weight Negative (Not allowed)
+    /*
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(100,20,"2"));
+    items.add(new Item(5000,-3,"3"));
+    level = new Level(items,amount,0,50);
+    expectedItems.add(items.get(0));
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+     */
+    // Value Negative
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(100,20,"2"));
+    items.add(new Item(-4,1,"3"));
+    level = new Level(items,amount,0,50);
+    expectedItems.add(items.get(0));
+    expectedItems.add(items.get(1));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Capacity Negative
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(130,20,"2"));
+    items.add(new Item(200,30,"3"));
+    level = new Level(items,amount,0,-10);
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Capacity Zero
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    items.add(new Item(60,10,"1"));
+    items.add(new Item(130,20,"2"));
+    items.add(new Item(200,30,"3"));
+    level = new Level(items,amount,0,0);
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+    // Randomly Sorted with 0 Weights
+    items = new ArrayList<>();
+    expectedItems = new ArrayList<>();
+    amount = new ArrayList<>();
+    amount.add(1);
+    amount.add(1);
+    amount.add(1);
+    amount.add(1);
+    amount.add(1);
+    amount.add(1);
+    amount.add(1);
+    items.add(new Item(60,0,"1"));
+    items.add(new Item(100,40,"2"));
+    items.add(new Item(120,30,"3"));
+    items.add(new Item(50,0,"4"));
+    items.add(new Item(70,0,"5"));
+    items.add(new Item(80,10,"6"));
+    items.add(new Item(80,80,"7"));
+    level = new Level(items,amount,0,80);
+    expectedItems.add(items.get(0));
+    expectedItems.add(items.get(1));
+    expectedItems.add(items.get(2));
+    expectedItems.add(items.get(4));
+    expectedItems.add(items.get(3));
+    expectedItems.add(items.get(5));
+    Assertions.assertEquals(SolverGreedy.sortLikeGreedy(expectedItems),SolverGreedy.sortLikeGreedy(solverBacktracking.solveAlgorithm(level)));
+  }
 
 }
