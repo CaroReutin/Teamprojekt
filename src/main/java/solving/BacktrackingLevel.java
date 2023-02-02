3b5988fc6685809522372baf58dfd7b32ab1ea3e
uhrkt <93714829+uhrkt@users.noreply.github.com>
2023-02-02 13:43:16 +0100

    added checkstyle

--- src/main/java/solving/BacktrackingLevel.java
+++ src/main/java/solving/BacktrackingLevel.java
@@ -1,8 +1,6 @@
 package solving;
 
 import java.io.File;
-import java.util.ArrayList;
-import rucksack.Item;
 import rucksack.Level;
 
 /**
@@ -26,39 +24,32 @@ public final class BacktrackingLevel {
    */
   public static void initializeBacktracking() {
     //Backtracking Level 1
-
-    LEVEL_BACKTRACKING[0] = AppData.loadLevel(new File("src/main/resources/level/Backtracking1.zip"));
-
+    LEVEL_BACKTRACKING[0] =
+      AppData.loadLevel(new File("src/main/resources/level/Backtracking1.zip"));
 
     //Backtracking Level 2
-
     LEVEL_BACKTRACKING[1] =
-        AppData.loadLevel(new File("src/main/resources/level/Backtracking2.zip"));
-
+      AppData.loadLevel(new File("src/main/resources/level/Backtracking2.zip"));
 
     //Backtracking Level 3
-
     LEVEL_BACKTRACKING[2] =
-        AppData.loadLevel(new File("src/main/resources/level/Backtracking3.zip"));
+      AppData.loadLevel(new File("src/main/resources/level/Backtracking3.zip"));
 
     //Backtracking Level 4
-
     LEVEL_BACKTRACKING[3] =
-        AppData.loadLevel(new File("src/main/resources/level/Backtracking4.zip"));
+      AppData.loadLevel(new File("src/main/resources/level/Backtracking4.zip"));
 
     //Backtracking Level 5
-
     LEVEL_BACKTRACKING[4] =
-        AppData.loadLevel(new File("src/main/resources/level/Backtracking5.zip"));
+      AppData.loadLevel(new File("src/main/resources/level/Backtracking5.zip"));
 
     //Backtracking Level 6
-
     LEVEL_BACKTRACKING[5] =
-        AppData.loadLevel(new File("src/main/resources/level/Backtracking6.zip"));
+      AppData.loadLevel(new File("src/main/resources/level/Backtracking6.zip"));
 
     //Backtracking Level 7
-
-    //LEVEL_BACKTRACKING[6] = AppData.loadLevel(new File("src/main/resources/level/Backtracking7.zip"));
+    //LEVEL_BACKTRACKING[6] =
+    //AppData.loadLevel(new File("src/main/resources/level/Backtracking7.zip"));
   }
 
   /**
